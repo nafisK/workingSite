b18665275e551fa9a7f438f9c9ef3f503dceb31d
nafisK <nafisrizwank@gmail.com>
2022-01-06 13:33:23 -0500

    finished intro description

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -66,7 +66,7 @@ class Header extends Component {
           <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
             <h3>
-              I'm an <span>{occupation}</span>.
+              <span>{occupation}</span>.
               <br />
               <br />
               {description}.
